af1202c8b5d31419f14ef436a6764a20d5d2c019
Kevin <Kevin22pr@gmail.com>
2020-09-16 14:25:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //initializes the default state.
+  state = {
+    apiResponse: ''
+  };
+
+  //fetch the data from the API and store the response on this.state.apiResponse.
+  callAPI(){
+    fetch("http://localhost:9000")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+  };
+  //execute the callAPI() method after the component mounts.
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
